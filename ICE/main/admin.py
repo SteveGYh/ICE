050bd28f66445a07fd16bbb85fc2684a7c42df07
Stevegaoyihan <48949703+Stevegaoyihan@users.noreply.github.com>
2020-11-26 01:14:47 -0500

    Initial update

---
+++ ICE/main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Course, Temp, ModuleT, Module
+# Register your models here.
+admin.site.register(Course)
+admin.site.register(Temp)
+admin.site.register(ModuleT)
+admin.site.register(Module)
